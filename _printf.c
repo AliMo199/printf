9d812085f4b1aef15bb4a70f24666d25f87b5738
Ali Elgamal <alimelgamal199@gmail.com>
2023-11-13 20:55:29 +0200

    Modified

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function.
+ * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
@@ -11,13 +11,13 @@ int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_char = 0;
 	int flags, width, precision, size, buff_ind = 0;
-	va_list1 list1;
+	va_list list;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(list1, format);
+	va_start(list, format);
 
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
@@ -33,11 +33,11 @@ int _printf(const char *format, ...)
 		{
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
-			width = get_width(format, &i, list1);
-			precision = get_precision(format, &i, list1);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list1, buffer,
+			printed = handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
@@ -47,15 +47,15 @@ int _printf(const char *format, ...)
 
 	print_buffer(buffer, &buff_ind);
 
-	va_end(list1);
+	va_end(list);
 
 	return (printed_char);
 }
 
 /**
- * print_buffer - Print the contents of buffer if it exist
- * @buffer: Array of char
- * @buff_ind: Index to add next char at, represents length.
+ * print_buffer - Print contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index to add next char at, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
